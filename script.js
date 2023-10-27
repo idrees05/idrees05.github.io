fb656954262d9e7462d4164dbd0c3eb3bf8adf16
Idrees Rahman <idreesrahman04@gmail.com>
2023-10-27 16:22:00 +0100

    Contact Me Button Added

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("contact-btn").addEventListener("click", function() {
+    // Select the footer element
+    var footer = document.querySelector("footer");
+  
+    // Scroll to the footer
+    footer.scrollIntoView({ behavior: "smooth" });
+  });
