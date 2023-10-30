bd092bafea0772ae4e44dbba66b4a62d01197844
Idrees Rahman <idreesrahman04@gmail.com>
2023-10-30 21:33:34 +0000

    Adding Buttons to my projects

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document.getElementById("contact-btn").addEventListener("click", function() {
     // Scroll to the footer
     footer.scrollIntoView({ behavior: "smooth" });
   });
+
+  document.getElementById('viewProjectBtnOne').addEventListener('click', function() {
+    window.open("https://github.com/idrees05/idrees05.github.io", "_blank");
+});
+
+document.getElementById('viewProjectBtnTwo').addEventListener('click', function() {
+    window.open("https://github.com/idrees05/Developer-Training/tree/main/Week%205%20Project/Weather%20Dashboard", "_blank");
+});
+
+document.getElementById('viewProjectBtnThree').addEventListener('click', function() {
+    window.open("https://github.com/idrees05/Developer-Training/tree/main/Week%205%20Project/To%20do%20List", "_blank");
+});
